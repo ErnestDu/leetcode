a4ae7f1fbbc654315e0e2fadf025109f478a4788
ErnestDu <hongqingdu@gmail.com>
2014-04-30 15:40:05 +0800

    new files

---
+++ validate-binary-search-tree.py
@@ -0,0 +1,10 @@
+class Solution:
+	def isValidBST(self, root):
+		return self.isValidBSTImpl(root, -2**31, 2**31 - 1)
+	
+	def isValidBSTImpl(self, root, min, max):
+		if root == None:
+			return True
+		return root.val > min and root.val < max \
+				and self.isValidBSTImpl(root.left, min, root.val) \
+				and self.isValidBSTImpl(root.right, root.val, max)
